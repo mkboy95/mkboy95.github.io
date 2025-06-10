049d221ffc1a4e33aa973a949497f35fc4a8b268
mkboy95 <1109008002@qq.com>
2025-06-11 00:19:42 +0800

    Site updated: 2025-06-11 00:19:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/16107/","posts/30914/"];function toRandomPost(){
+var posts=["posts/16107/","posts/30914/","posts/b48e/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

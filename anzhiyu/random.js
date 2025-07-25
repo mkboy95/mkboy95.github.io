46f61c08c896dd2f006bded62b2fb23a05151c1d
mkboy95 <1109008002@qq.com>
2025-07-26 01:05:23 +0800

    Site updated: 2025-07-26 01:05:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/16107/","posts/30914/","posts/54d0/","posts/2f34/","posts/b48e/"];function toRandomPost(){
+var posts=["posts/2f34/","posts/54d0/","posts/b48e/","posts/b48e/","posts/30914/","posts/16107/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
